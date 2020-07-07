4a451f05a19abfdbd434921ec54ebebee6863920
Dotan Schachter <contac@dotan.me>
2020-07-07 19:34:55 -0400

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Dotan Schachter`,
+      summary: `who lives on Earth.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
